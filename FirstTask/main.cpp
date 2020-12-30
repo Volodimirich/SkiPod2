5e0bbbbfb56f00239f7c542b4571639a2d410192
Volodimirich <shaposh-vova@rambler.ru>
2020-12-31 00:30:39 +0300

    Final version

--- FirstTask/main.cpp
+++ FirstTask/main.cpp
@@ -3,83 +3,79 @@
 #include <mpi.h>
 #include <random>
 
-int get_resieve_amount(int rank)
-{
-    switch (rank%4) {
-        case 1:
-            return 2;
-        case 2:
-            return 1;
-        case 3:
-            return 0;
-        case 0:
-            switch (rank) {
-                case 4:
-                    return 11;
-                case 8:
-                    return 7;
-                case 12:
-                    return 3;
-                case 0:
-                    return 0;
-                default:
-                    throw;
-            }
-    }
-    throw;
-}
+std::map<int, int> send_to = {{1,0}, {2,1}, {3,2},{4,0}, {5,1}, {6,2},
+                              {7, 6}, {8, 4}, {9,5}, {10, 9}, {11, 10},
+                              {12, 8}, {13, 12}, {14,13}, {15, 14}};
 
-int get_send(int rank)
-{
-    if (rank%4)
-        return rank-1;
-    else
-        return rank-4;
-}
+std::map<int, int> get_from = {{4, 8}, {5, 9}, {6,7}, {8, 12}, {9, 10}, {10,11},
+                               {12, 13}, {13, 14}, {14,15}};
 
-static void send_resieve (int rank)
+std::map<int, int> resieve_amount = {{3, 0}, {4,5}, {5, 3}, {6,1}, {7, 0}, {8, 4},
+                                     {9,2}, {10, 1}, {11, 0}, {12, 3}, {13,2},
+                                     {14,1}, {15,0}};
+
+static void send_resieve (int rank, std::vector<int> some_data = {})
 {
-    int resv_amount = get_resieve_amount(rank);
+
+
+    int resv_amount;
     std::vector <int> data_array;
-    int vec_size, send_to;
+    std::vector <int> result = some_data;
+    int vec_size;
     MPI_Status status;
 
+    if (rank == 1) {
 
-    if (rank%4 or rank==12) {
-        for (size_t i = 0; i < resv_amount; i++) {
-            send_to = rank==12 ? 8: rank-1;
-            MPI_Recv(&vec_size, 1, MPI_INT, rank + 1, (rank + 1) * 10 + rank + 1, MPI_COMM_WORLD, &status);
+        for (size_t i=0; i<4; i++) {
+            MPI_Recv(&vec_size, 1, MPI_INT, 5, 5 * 10 + 5, MPI_COMM_WORLD, &status);
             data_array.resize(vec_size);
-            MPI_Recv(&data_array[0], vec_size, MPI_INT, rank + 1, rank+1, MPI_COMM_WORLD, &status);
-
-            MPI_Send(&vec_size, 1, MPI_INT, send_to, rank * 10 + rank, MPI_COMM_WORLD);
-            MPI_Send(data_array.data(), vec_size, MPI_INT, send_to, rank, MPI_COMM_WORLD);
+            MPI_Recv(&data_array[0], vec_size, MPI_INT, 5, 5, MPI_COMM_WORLD, &status);
+            result.insert(result.end(), data_array.begin(), data_array.end());
             data_array.clear();
         }
+        MPI_Recv(&vec_size, 1, MPI_INT, 2, 2 * 10 + 2, MPI_COMM_WORLD, &status);
+        data_array.resize(vec_size);
+        MPI_Recv(&data_array[0], vec_size, MPI_INT, 2, 2, MPI_COMM_WORLD, &status);
+        result.insert(result.end(), data_array.begin(), data_array.end());
+
+        vec_size = result.size();
+        MPI_Send(&vec_size, 1, MPI_INT, 0, 1 * 10 + 1, MPI_COMM_WORLD);
+        MPI_Send(result.data(), vec_size, MPI_INT, 0, 1, MPI_COMM_WORLD);
+        data_array.clear();
+        result.clear();
+
+    } if (rank == 2) {
+        for (size_t i=0; i<2; i++) {
+            MPI_Recv(&vec_size, 1, MPI_INT, 6, 6 * 10 + 6, MPI_COMM_WORLD, &status);
+            data_array.resize(vec_size);
+            MPI_Recv(&data_array[0], vec_size, MPI_INT, 6, 6, MPI_COMM_WORLD, &status);
+            result.insert(result.end(), data_array.begin(), data_array.end());
+            data_array.clear();
+        }
+        MPI_Recv(&vec_size, 1, MPI_INT, 3, 3 * 10 + 3, MPI_COMM_WORLD, &status);
+        data_array.resize(vec_size);
+        MPI_Recv(&data_array[0], vec_size, MPI_INT, 3, 3, MPI_COMM_WORLD, &status);
+        result.insert(result.end(), data_array.begin(), data_array.end());
+
+
+        vec_size = result.size();
+        MPI_Send(&vec_size, 1, MPI_INT, 1, 2 * 10 + 2, MPI_COMM_WORLD);
+        MPI_Send(result.data(), vec_size, MPI_INT, 1, 2, MPI_COMM_WORLD);
+        data_array.clear();
+        result.clear();
     } else {
-        std::vector<int> sec_array;
-
-        for (size_t i=0; i< 3; i++) {
-            MPI_Recv(&vec_size, 1, MPI_INT, rank + 1, (rank + 1) * 10 + rank + 1, MPI_COMM_WORLD, &status);
+        resv_amount = resieve_amount[rank];
+        for (size_t i=0; i< resv_amount; i++) {
+            MPI_Recv(&vec_size, 1, MPI_INT, get_from[rank], get_from[rank] * 10 + get_from[rank], MPI_COMM_WORLD, &status);
             data_array.resize(vec_size);
-            MPI_Recv(&data_array[0], vec_size, MPI_INT, rank + 1, rank + 1, MPI_COMM_WORLD, &status);
+            MPI_Recv(&data_array[0], vec_size, MPI_INT, get_from[rank], get_from[rank], MPI_COMM_WORLD, &status);
 
-            MPI_Send(&vec_size, 1, MPI_INT, rank - 4, rank * 10 + rank, MPI_COMM_WORLD);
-            MPI_Send(data_array.data(), vec_size, MPI_INT, rank - 4 , rank, MPI_COMM_WORLD);
+            MPI_Send(&vec_size, 1, MPI_INT, send_to[rank], rank * 10 + rank, MPI_COMM_WORLD);
+            MPI_Send(data_array.data(), vec_size, MPI_INT, send_to[rank], rank, MPI_COMM_WORLD);
             data_array.clear();
-
-        }
-        for (size_t i = 0; i < (rank==4 ? 8: 4); i++) {
-            MPI_Recv(&vec_size, 1, MPI_INT, rank + 4, (rank + 4) * 10 + rank + 4, MPI_COMM_WORLD, &status);
-            data_array.resize(vec_size);
-            MPI_Recv(&data_array[0], vec_size, MPI_INT, rank + 4, rank+4, MPI_COMM_WORLD, &status);
-
-            MPI_Send(&vec_size, 1, MPI_INT, rank - 4, rank * 10 + rank, MPI_COMM_WORLD);
-            MPI_Send(data_array.data(), vec_size, MPI_INT, rank-4, rank, MPI_COMM_WORLD);
-            data_array.clear();
-
         }
     }
+
 }
 
 
@@ -94,32 +90,31 @@ int main (int argc, char* argv[])
     MPI_Status status;
     std::vector<int> data_array(rank);
     int vec_size;
-    int send_to;
-    resv_amount = get_resieve_amount(rank);
 
 
     if (rank != 0) {
-        for (size_t i=0; i<rank; i++) {
+        for (size_t i = 0; i < rank; i++) {
             data_array[i] = rank;
         }
 
-        send_to = rank%4 ? rank-1 : rank-4;
+
         vec_size = data_array.size();
-        MPI_Send(&vec_size,  1, MPI_INT, send_to, rank*10 + rank, MPI_COMM_WORLD);
-        MPI_Send(data_array.data(), vec_size, MPI_INT, send_to, rank, MPI_COMM_WORLD);
+        if (rank != 1 and rank != 2) {
+            MPI_Send(&vec_size, 1, MPI_INT, send_to[rank], rank * 10 + rank, MPI_COMM_WORLD);
+            MPI_Send(data_array.data(), vec_size, MPI_INT, send_to[rank], rank, MPI_COMM_WORLD);
+        }
         send_resieve(rank);
 
     } else {
         std::vector <std::vector <int>> result_data;
-        for (size_t i=0; i< 3; i++) {
-            MPI_Recv(&vec_size, 1, MPI_INT, 1, 11, MPI_COMM_WORLD, &status);
-            data_array.resize(vec_size);
-            MPI_Recv(&data_array[0], vec_size, MPI_INT, 1, 1, MPI_COMM_WORLD, &status);
-            result_data.emplace_back(data_array);
-            printf("Get data with %d\n", data_array[0]);
-            data_array.clear();
-        }
-        for (size_t i = 0; i < 12; i++) {
+        MPI_Recv(&vec_size, 1, MPI_INT, 1, 11, MPI_COMM_WORLD, &status);
+        data_array.resize(vec_size);
+        MPI_Recv(&data_array[0], vec_size, MPI_INT, 1, 1, MPI_COMM_WORLD, &status);
+        result_data.emplace_back(data_array);
+        printf("Get data with %d\n", data_array[0]);
+        data_array.clear();
+
+        for (size_t i = 0; i < 6; i++) {
             MPI_Recv(&vec_size, 1, MPI_INT, 4, 44, MPI_COMM_WORLD, &status);
             data_array.resize(vec_size);
             MPI_Recv(&data_array[0], vec_size, MPI_INT, 4, 4, MPI_COMM_WORLD, &status);
